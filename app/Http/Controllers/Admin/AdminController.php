f472152a6474b520e057f6fbe9d879bd80f3660c
namdinh247 <hanoimatbao@gmail.com>
2020-08-16 23:35:46 +0700

    thêm các mục

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -24,4 +24,48 @@ class AdminController extends Controller
     public function detailAccount(){
         return view('admin.detailAccount');
     }
+
+    // category
+    public function listCategory(){
+        return view('admin.listCategory');
+    }
+    public function newCategory(){
+        return view('admin.newCategory');
+    }
+    public function detailCategory(){
+        return view('admin.detailCategory');
+    }
+
+    // product
+    public function listProduct(){
+        return view('admin.listProduct');
+    }
+    public function newProduct(){
+        return view('admin.newProduct');
+    }
+    public function detailProduct(){
+        return view('admin.detailProduct');
+    }
+
+    // orders
+    public function listOrders(){
+        return view('admin.listOrders');
+    }
+    public function newOrders(){
+        return view('admin.newOrders');
+    }
+    public function detailOrders(){
+        return view('admin.detailOrders');
+    }
+
+    // posts
+    public function listPosts(){
+        return view('admin.listPosts');
+    }
+    public function newPosts(){
+        return view('admin.newPosts');
+    }
+    public function detailPosts(){
+        return view('admin.detailPosts');
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -21,3 +21,31 @@ Route::get('/admin/account', 'Admin\AdminController@accountManagement');
 Route::get('/admin/account/new', 'Admin\AdminController@newAccount');
 
 Route::get('/admin/account/detail', 'Admin\AdminController@detailAccount');
+
+// category
+Route::get('/admin/category', 'Admin\AdminController@listCategory');
+
+Route::get('/admin/category/new', 'Admin\AdminController@newCategory');
+
+Route::get('/admin/category/detail', 'Admin\AdminController@detailCategory');
+
+// product
+Route::get('/admin/product', 'Admin\AdminController@listProduct');
+
+Route::get('/admin/product/new', 'Admin\AdminController@newProduct');
+
+Route::get('/admin/product/detail', 'Admin\AdminController@detailProduct');
+
+// orders
+Route::get('/admin/orders', 'Admin\AdminController@listOrders');
+
+Route::get('/admin/orders/new', 'Admin\AdminController@newOrders');
+
+Route::get('/admin/orders/detail', 'Admin\AdminController@detailOrders');
+
+// posts
+Route::get('/admin/posts', 'Admin\AdminController@listPosts');
+
+Route::get('/admin/posts/new', 'Admin\AdminController@newPosts');
+
+Route::get('/admin/posts/detail', 'Admin\AdminController@detailPosts');
