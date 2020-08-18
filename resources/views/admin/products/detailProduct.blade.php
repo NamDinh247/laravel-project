f362fc430a31f41a0861d7bc15ce1df51d604542
namdinh247 <hanoimatbao@gmail.com>
2020-08-19 00:05:35 +0700

    hoàn thiện giao diện sản phẩm

--- resources/views/admin/products/detailProduct.blade.php
+++ resources/views/admin/products/detailProduct.blade.php
@@ -7,100 +7,112 @@
 @section('main-content')
     <div class="row">
         <div class="col-md-12 mb-4">
-            <a href="/admin/account" class="gobacklist"><i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp; Danh sách tài khoản</a>
+            <a href="/admin/product" class="gobacklist"><i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp; Danh sách sản phẩm</a>
         </div>
-        <div class="col-12">
-            <div class="container bootstrap snippet">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <div class="text-center">
-                            <img src="/img/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar" style="border-radius: 50% !important;width: 13rem;height: 13rem; border: 2px solid #20c997;">
-                            <input type="file" class="text-center center-block file-upload" style="margin-top: 20px;">
+        <div class="col-md-12">
+            <div class="card border-r-0 border-0">
+                <div class="card-body container bootstrap snippet">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h5>Thêm mới sản phẩm</h5>
+                            <hr/>
                         </div>
-                    </div>
-                    <div class="col-sm-9">
-                        <h5>Thông tin chung</h5>
-                        <hr/>
-                        <form class="form row" action="#" method="post" id="accountForm">
-                            <div class="form-group col-md-6">
-                                <label>Tên tài khoản</label>
-                                <input type="text" class="form-control" id="userName" placeholder="Tên tài khoản">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label>Loại tài khoản</label>
-                                <select id="type-account" class="form-control">
-                                    <option value="">Chọn loại tài khoản</option>
-                                    <option value="user">Người dùng</option>
-                                    <option value="shop">Cửa hàng</option>
-                                    <option value="admin">Admin</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group col-md-6">
-                                <label>Số điện thoại</label>
-                                <input type="text" class="form-control" name="phone" id="phone" placeholder="Nhập số điện thoại">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="password">Mật khẩu</label>
-                                <input type="password" class="form-control" id="password" placeholder="Mật khẩu">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label>Kiểm chứng</label>
-                                <input type="password" class="form-control" id="password2" placeholder="Nhập lại mật khẩu">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label>Trạng thái</label>
-                                <select id="status" class="form-control">
-                                    <option value="active">Hoạt động</option>
-                                    <option value="inactive">Không hoạt động</option>
-                                </select>
-                            </div>
-                            {{-- show --}}
-                            <div class="form-group content-bottom-detail col-md-6 d-none">
-                                <label>Loại sản phẩm</label>
-                                <select id="type-product" class="form-control">
-                                    <option value="">Chọn loại sản phẩm</option>
-                                    <option value="metal">Kim loại</option>
-                                    <option value="wood">Gỗ</option>
-                                    <option value="rubber">Nhựa, cao su</option>
-                                    <option value="glass">Thuỷ tinh</option>
-                                    <option value="synthetic">Vật liệu tổng hợp</option>
-                                    <option value="other">Khác</option>
-                                </select>
-                            </div>
-                            <div class="form-group content-bottom-detail col-md-6 d-none">
-                                <label>Địa chỉ</label>
-                                <input type="text" class="form-control" id="address" placeholder="Nhập địa chỉ">
-                            </div>
-                            <div class="form-group content-bottom-detail col-md-6 d-none">
-                                <label>Số tài khoản ngân hàng</label>
-                                <input type="text" class="form-control" id="number_bank" placeholder="Nhập số tài khoản">
-                            </div>
-                            <div class="form-group content-bottom-detail col-md-6 d-none">
-                                <label>Ngân hàng</label>
-                                <input type="text" class="form-control" id="bank" placeholder="Nhập tên ngân hàng">
-                            </div>
-                            <div class="form-group col-md-12">
-                                <label>Ghi chú</label>
-                                <textarea type="text" class="form-control" id="address" placeholder="Nhập ghi chú" style="resize: vertical;"></textarea>
-                            </div>
-                            <div class="form-group col-md-12">
-                                <div class="col-md-6 p-0">
-                                    <button class="btn btn-lg btn-success" type="submit"><i class="fa fa-save"></i>&nbsp; Lưu</button>
+                        <div class="col-sm-8">
+                            <form class="form row" action="#" method="post" id="accountForm">
+                                <div class="form-group col-md-6">
+                                    <label>Mã sản phẩm</label>
+                                    <input type="text" class="form-control" id="code_product" placeholder="Mã sản phẩm">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Tên tài khoản</label>
+                                    <input type="text" class="form-control" id="userName" placeholder="Tên sản phẩm">
+                                </div>
+                                {{-- danh mục này lấy từ category --}}
+                                <div class="form-group col-md-6">
+                                    <label>Loại sản phẩm</label>
+                                    <select id="type-product" class="form-control">
+                                        <option value="">Chọn loại sản phẩm</option>
+                                        {{-- đây là dữ liệu mẫu, for từ đây, xong thì xoá comment này --}}
+                                        <option value="user">Kim loại</option>
+                                        <option value="shop">Gỗ</option>
+                                        <option value="admin">Nhựa, cao su</option>
+                                    </select>
+                                </div>
+                                {{-- tìm hiểu và sử dụng select2 ở đây --}}
+                                <div class="form-group col-md-6">
+                                    <label>Cửa hàng</label>
+                                    <select id="select_shop" class="form-control">
+                                        {{-- đây là dữ liệu mẫu, for từ đây, xong thì xoá comment này --}}
+                                        <option value="user">Kim loại</option>
+                                        <option value="shop">Gỗ</option>
+                                        <option value="admin">Nhựa, cao su</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Giá bán (VND)</label>
+                                    <input type="number" class="form-control" min="0" id="price" placeholder="0">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Số lượng</label>
+                                    <input type="number" class="form-control" id="quantily" min="1" placeholder="1">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Giảm giá</label>
+                                    <select id="sale_shop" class="form-control">
+                                        <option value="user" selected>Có</option>
+                                        <option value="shop">Không</option>
+                                    </select>
+                                </div>
+                                {{-- show --}}
+                                <div class="form-group col-md-6">
+                                    <label>Giá trị giảm giá</label>
+                                    <input type="number" class="form-control" id="percent" min="0" max="100" placeholder="0">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Giá bán sau giảm giá</label>
+                                    <input type="number" class="form-control" id="price2" placeholder="0">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label>Trạng thái</label>
+                                    <select id="sale_shop" class="form-control">
+                                        <option value="active">Hoạt động</option>
+                                        <option value="inactive">Không hoạt động</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label>Ghi chú</label>
+                                    <textarea type="text" class="form-control" id="address" placeholder="Nhập ghi chú" style="resize: vertical;"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-md-12">
+                                        <button class="btn btn-lg btn-success" type="submit"><i class="fa fa-save"></i>&nbsp; Lưu</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md-4">
+                            <label for="">Tải ảnh lên</label>
+                            <input type="file" id="uploadImages" accept="image/*" multiple>
+                            <div class="images_product pt-5">
+                                <div class="row">
+                                    {{-- for ở đây --}}
+                                    <div class="col-md-4">
+                                        <div class="image_product position-relative">
+                                            <img src="" alt="">
+                                            <i class="position-absolute fa fa-trash" style="top: 0; right: 0;font-size: 13px; color: red;"></i>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
 @section('main-script')
     <script src="/Admin/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="/js/detailAccount.js"></script>
+    <script src="/js/product/detailProduct.js"></script>
 @endsection
--- resources/views/admin/products/listProduct.blade.php
+++ resources/views/admin/products/listProduct.blade.php
@@ -3,36 +3,86 @@
 @section('header-script')
     <link rel="stylesheet" href="/Admin/plugins/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/Admin/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="/Admin/plugins/ion-rangeslider/css/ion.rangeSlider.min.css">
 @endsection
 
 @section('main-content')
     <div class="row">
+        <div class="col-md-12">
+            <div class="card border-0 border-r-0">
+                <div class="card-body border-0 clearfix">
+                    {{-- search --}}
+                    <div class="input-group input-group-sm mr-1 float-left" style="width: 200px;">
+                        <input type="text" name="table_search" class="form-control" placeholder="Tìm kiếm" style="border-radius: 0 !important;">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-default" style="border: 1px solid #ced4da; border-radius: 0 !important;"><i class="fas fa-search"></i></button>
+                        </div>
+                    </div>
+                    {{-- status --}}
+                    <div class="dropdown mr-1 ml-1 float-left">
+                        <a class="btn dropdown-toggle bg-white style_dropdown" role="button" id="filter_status" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Trạng thái</a>
+                        <div class="dropdown-menu" aria-labelledby="filter_status">
+                            <a class="dropdown-item active">Tất cả</a>
+                            <a class="dropdown-item">Mới</a>
+                            <a class="dropdown-item">Còn hàng</a>
+                            <a class="dropdown-item">Hết hàng</a>
+                            <a class="dropdown-item">Tồn kho</a>
+                            <a class="dropdown-item">Dừng bán</a>
+                        </div>
+                    </div>
+                    {{-- type product --}}
+                    <div class="dropdown mr-1 ml-1 float-left">
+                        <a class="btn dropdown-toggle bg-white style_dropdown" role="button" id="filter_type" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Loại sản phẩm</a>
+                        <div class="dropdown-menu" aria-labelledby="filter_type">
+                            <a class="dropdown-item active">Tất cả</a>
+                            <a class="dropdown-item">Kim loại</a>
+                            <a class="dropdown-item">Gỗ</a>
+                            <a class="dropdown-item">Nhựa, cao su</a>
+                            <a class="dropdown-item">Thuỷ tinh</a>
+                            <a class="dropdown-item">Khác</a>
+                        </div>
+                    </div>
+                    {{-- quantily --}}
+                    <div class="dropdown mr-1 ml-1 float-left">
+                        <a class="btn dropdown-toggle bg-white style_dropdown" role="button" id="filter_quantily" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Số lượng</a>
+                        <div class="dropdown-menu" style="width: 150px" aria-labelledby="filter_quantily">
+                            <div class="pl-2 pr-2">
+                                <input type="number" class="form-control border-r-0" min="0" placeholder="0"/>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- giảm giá --}}
+                    <div class="dropdown mr-1 ml-1 float-left">
+                        <a class="btn dropdown-toggle bg-white style_dropdown" role="button" id="filter_type" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Giảm giá</a>
+                        <div class="dropdown-menu" style="width: 200px" aria-labelledby="filter_type">
+                            <div class="p-3">
+                                <input id="range_1" class="p-3" type="text" name="range_1" value="">
+                            </div>
+                        </div>
+                    </div>
+                    {{-- giá bán --}}
+                    <div class="dropdown mr-1 ml-1 float-left">
+                        <a class="btn dropdown-toggle bg-white style_dropdown" role="button" id="filter_type" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Giá bán</a>
+                        <div class="dropdown-menu border-r-0" style="width: 150px;" aria-labelledby="filter_type">
+                            <div class="p-3">
+                                <label>Từ:  <input class="form-control" type="number" min="0" placeholder="0"></label>
+                                <label class="pt-2 pb-2">Đến:  <input class="form-control" type="number" min="0" placeholder="0"></label>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- date --}}
+                    <div class="input-group mr-1 ml-1 float-left" style="width: 250px;">
+                        <input type="text" class="form-control" readonly="" id="dateTime" style="border-radius: 0 !important;"/>
+                        <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="col-12">
-            <div class="content-table bg-white">
+            <div class="card content-table bg-white">
                 <div class="card-header bg-white position-relative border-0">
                     <h4 class="card-title" style="margin-bottom: 0 !important;">Danh sách sản phẩm</h4>
                     <div class="breadcrumb">
-                        <div class="input-group input-group-sm" style="width: 200px;">
-                            <input type="text" name="table_search" class="form-control" placeholder="Tìm kiếm" style="border-radius: 0 !important;">
-                            <div class="input-group-append">
-                                <button type="submit" class="btn btn-default" style="border: 1px solid #ced4da; border-radius: 0 !important;"><i class="fas fa-search"></i></button>
-                            </div>
-                        </div>
-                        <div class="input-group mr-1 ml-1" style="width: 250px;">
-                            <input type="text" class="form-control" readonly="" id="dateTime" style="border-radius: 0 !important;"/>
-                            <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-                        </div>
-                        <div class="input-group mr-1 ml-1" style="width: 200px;">
-                            <select class="form-control" style="border-radius: 0 !important;">
-                                <option value="">Tất cả</option>
-                                <option value="new">Mới</option>
-                                <option value="stocking">Còn hàng</option>
-                                <option value="outofstock">Hết hàng</option>
-                                <option value="inventory">Tồn kho</option>
-                                <option value="sale">Giảm giá</option>
-                                <option value="stop">Dừng bán</option>
-                            </select>
-                        </div>
                         <a href="/admin/product/new" type="button" class="btn btn-sm btn-success"><i class="fa fa-plus"></i>&nbsp; Thêm mới</a>
                     </div>
                 </div>
@@ -51,7 +101,15 @@
                             <th class="text-xl-right ver-middle">Giá bán</th>
                             <th class="ver-middle">Số lượng</th>
                             <th class="ver-middle">Trạng thái</th>
-                            <th class="ver-middle"></th>
+                            <th class="ver-middle">Ngày tạo</th>
+                            <th class="ver-middle">
+                                <div class="dropdown">
+                                    <a class="btn dropdown-toggle bg-white" style="border: 1px solid #ced4da !important; padding: .3rem .75rem !important; border-radius: 0 !important;" role="button" id="bulkaction_product" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Thao tác nhiều</a>
+                                    <div class="dropdown-menu" aria-labelledby="bulkaction_product">
+                                        <a class="dropdown-item active">Xoá</a>
+                                    </div>
+                                </div>
+                            </th>
                         </tr>
                         </thead>
                         <tbody>
@@ -67,6 +125,7 @@
                             <td class="ver-middle">300.000 VND</td>
                             <td class="ver-middle">2</td>
                             <td class="ver-middle">Còn hàng</td>
+                            <td class="ver-middle">18/8/2020</td>
                             <td class="text-xl-right ver-middle">
                                 <a href="/admin/product/detail" type="button" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i>&nbsp; Sửa</a>
                                 <button type="button" class="btn btn-sm btn-danger" value="1" onclick="showModalDeleteProduct(this)"><i class="fa fa-trash"></i>&nbsp; Xoá</button>
@@ -84,6 +143,7 @@
                             <td class="ver-middle">300.000 VND</td>
                             <td class="ver-middle">2</td>
                             <td class="ver-middle">Còn hàng</td>
+                            <td class="ver-middle">18/8/2020</td>
                             <td class="text-xl-right ver-middle">
                                 <a href="/admin/product/detail" type="button" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i>&nbsp; Sửa</a>
                                 <button type="button" class="btn btn-sm btn-danger" value="2" onclick="showModalDeleteProduct(this)"><i class="fa fa-trash"></i>&nbsp; Xoá</button>
@@ -101,6 +161,7 @@
                             <td class="ver-middle">300.000 VND</td>
                             <td class="ver-middle">2</td>
                             <td class="ver-middle">Còn hàng</td>
+                            <td class="ver-middle">18/8/2020</td>
                             <td class="text-xl-right ver-middle">
                                 <a href="/admin/product/detail" type="button" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i>&nbsp; Sửa</a>
                                 <button type="button" class="btn btn-sm btn-danger" value="3" onclick="showModalDeleteProduct(this)"><i class="fa fa-trash"></i>&nbsp; Xoá</button>
@@ -118,6 +179,7 @@
                             <td class="ver-middle">300.000 VND</td>
                             <td class="ver-middle">2</td>
                             <td class="ver-middle">Còn hàng</td>
+                            <td class="ver-middle">18/8/2020</td>
                             <td class="text-xl-right ver-middle">
                                 <a href="/admin/product/detail" type="button" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i>&nbsp; Sửa</a>
                                 <button type="button" class="btn btn-sm btn-danger" value="4" onclick="showModalDeleteProduct(this)"><i class="fa fa-trash"></i>&nbsp; Xoá</button>
@@ -135,6 +197,7 @@
                             <td class="ver-middle">300.000 VND</td>
                             <td class="ver-middle">2</td>
                             <td class="ver-middle">Còn hàng</td>
+                            <td class="ver-middle">18/8/2020</td>
                             <td class="text-xl-right ver-middle">
                                 <a href="/admin/product/detail" type="button" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i>&nbsp; Sửa</a>
                                 <button type="button" class="btn btn-sm btn-danger" value="5" onclick="showModalDeleteProduct(this)"><i class="fa fa-trash"></i>&nbsp; Xoá</button>
@@ -171,7 +234,7 @@
 @endsection
 @section('modal')
     <!-- Modal -->
-    <div class="modal fade" id="modal-delete-account">
+    <div class="modal fade" id="modal-delete-product">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header" style="background-color: #20c997;color: #fff;">
@@ -194,8 +257,9 @@
 @section('main-script')
     <script src="/Admin/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="/Admin/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="/Admin/plugins/ion-rangeslider/js/ion.rangeSlider.min.js"></script>
     <script src="/Admin/plugins/moment/moment.min.js"></script>
     <script src="/Admin/plugins/moment/locale/vi.js"></script>
-    <script src="/js/product.js"></script>
+    <script src="/js/product/product.js"></script>
 
 @endsection
